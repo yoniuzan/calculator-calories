a1c40c7ebabef1e8ef321db9638ea63baa1e0642
YonatanUzan <Yonatan@funatix.club>
2022-09-13 11:37:26 +0300

    change

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ let express = require('express');
 
 let app = express();
 
+let port = process.env.PORT || 8080;
+
 app.use(express.static(__dirname+'/dist/calculator-calories'));
 
 app.get('/*', (req, resp) => {
     resp.sendFile(__dirname+'/dist/calculator-calories/index.html');
 })
 
-app.listen(process.env.PORT || 8080);
+app.listen(port);
